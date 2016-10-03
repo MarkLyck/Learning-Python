bf4f30e2702a4c999c09395ebf6927d88a142d57
Mark Lyck <mark.lyck@gmail.com>
2016-10-02 20:18:23 -0500

    More functions

--- functions.py
+++ functions.py
@@ -6,6 +6,32 @@ def bitcoin_to_usd(btc):
     amount = btc * 527
     return amount
 
-beef()
-amount = bitcoin_to_usd(3.85)
-print(amount)
+# beef()
+# amount = bitcoin_to_usd(3.85)
+# print(amount)
+#
+
+# default variables
+
+def getgender(sex='unknown'):
+    if sex is 'm':
+        sex = 'male'
+    elif sex is 'f':
+        sex = 'female'
+    print(sex)
+
+
+# getgender('m')
+# getgender('f')
+# getgender()
+
+#Keyword arguments
+
+def dumb_sentence(name="Mark", action="ate", item="tuna"):
+    print(name, action, item)
+
+dumb_sentence()
+dumb_sentence('Sally', 'farts', 'gently')
+dumb_sentence(item='awesome', action="is")
+
+#
