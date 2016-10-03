36b1b39b8b4517a0d06e7c2ee2f9ec4a16f7403f
Mark Lyck <mark.lyck@gmail.com>
2016-10-02 19:19:07 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+name = 'Lucy'
+
+if name is 'Bucky':
+    print('Hey there Bucky')
+elif name is 'Lucy':
+    print('Hey there Lucy')
+else:
+    print('Pleae sign up for the site')
---
+++ range_while.py
@@ -0,0 +1,8 @@
+# for x in range(10, 40, 5):
+    # print(x)
+
+tuna = 5
+
+while tuna < 10:
+    print(tuna)
+    tuna += 1
