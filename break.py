82d0c7c2183ce124f2bf4fabfcb065f522bada80
Mark Lyck <mark.lyck@gmail.com>
2016-10-02 19:29:02 -0500

    Added loop breaks, continue and functions

---
+++ break.py
@@ -0,0 +1,10 @@
+magicNumber = 26
+
+# this program finds a magic number
+
+for n in range(101):
+    if n is magicNumber:
+        print(n, ' is the magic number')
+        break
+    else:
+        print(n)
